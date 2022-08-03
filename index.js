c81554387c50a2a8eedd15388b0168e77f866d98
Andrew Chernishev <48554450+bollgade@users.noreply.github.com>
2022-08-03 17:09:30 +0300

    update

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
+const axios = require('axios');
 const config = require('./config.json');
+const fs = require('fs');
 const urls = [
   'https://agronom.platrum.ru/wiki/api/article/list',
   'https://agronom.platrum.ru/wiki/api/article/report'
 ];
-const url = urls[0];
-const axios = require('axios');
 const articles = [];
-const links = [];
 
 const fetch = async (url) => {
   const response = await axios.get(url, { headers: { 'Api-key': config['Api-key'] } });
   setArticles(response.data.data);
-  console.log(response.data.data);
 };
-fetch(url);
+fetch(urls[0]);
 
 const setArticles = (data) => {
   // el.articles.forEach(el =>)
   data.forEach(el => articles.push(el));
   // setLinks(articles);
-  console.log(articles[0]);
+  console.log(articles[2]);
   // console.log(data);
 };
 
+function createFile(article) {
 
-// const setLinks = (data) => {
-//   data.forEach(el => {
-//     links.push(`${el['article_id']}-${el.slug}`);
-//   })
-//   console.log(links);
-// }
+}
